f64064500f695fff4ff2f5b1c6ea512b1679eb92
wpghksdnd <oahfaj@naver.com>
2023-12-01 10:56:59 +0900

    메세지기능

--- DBP_TeamProject/Forms/FormMessage.Designer.cs
+++ DBP_TeamProject/Forms/FormMessage.Designer.cs
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FormMessage";
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            listBox1 = new ListBox();
+            label4 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(640, 27);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 0;
+            button1.Text = "보내기";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(640, 124);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 1;
+            button2.Text = "검색";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(227, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(525, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(161, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(66, 25);
+            label1.TabIndex = 3;
+            label1.Text = "수신자";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(227, 32);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(414, 33);
+            comboBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(161, 78);
+            label2.Name = "label2";
+            label2.Size = new Size(48, 25);
+            label2.TabIndex = 5;
+            label2.Text = "제목";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(161, 124);
+            label3.Name = "label3";
+            label3.Size = new Size(48, 25);
+            label3.TabIndex = 7;
+            label3.Text = "검색";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(227, 124);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(414, 31);
+            textBox2.TabIndex = 6;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 168);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(740, 258);
+            richTextBox1.TabIndex = 8;
+            richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(12, 465);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(740, 129);
+            listBox1.TabIndex = 9;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 437);
+            label4.Name = "label4";
+            label4.Size = new Size(108, 25);
+            label4.TabIndex = 10;
+            label4.Text = "수신 메세지";
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // FormMessage
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 629);
+            Controls.Add(label4);
+            Controls.Add(listBox1);
+            Controls.Add(richTextBox1);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(comboBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "FormMessage";
+            Text = "FormMessage";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label1;
+        private ComboBox comboBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private RichTextBox richTextBox1;
+        private ListBox listBox1;
+        private Label label4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- DBP_TeamProject/Forms/FormMessage.cs
+++ DBP_TeamProject/Forms/FormMessage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,316 @@ namespace DBP_TeamProject.Forms
 {
     public partial class FormMessage : Form
     {
+
+        DBManager dbManager = DBManager.GetInstance();
+        Query query = Query.GetInstance();
+        LoginedUser loginedUser = LoginedUser.getInstance();
+        private System.Windows.Forms.Timer messageCheckTimer;
+        private bool isNotificationShown = false;
+
         public FormMessage()
         {
             InitializeComponent();
+
+            PopulateComboBox();
+            ShowReceivedMessagesForUser(loginedUser.UserId);//자동으로 로그인한 유저 정보 받게 설정////////////////////////////////////////////////////////////////
+
+            // 타이머 초기화 및 설정
+            messageCheckTimer = new System.Windows.Forms.Timer();
+            messageCheckTimer.Interval = 5000; // 5초마다 확인
+            messageCheckTimer.Tick += new EventHandler(MessageCheckTimer_Tick);
+            messageCheckTimer.Start();
+        }
+
+
+        private void MessageCheckTimer_Tick(object sender, EventArgs e)
+        {
+            ShowReceivedMessagesForUser(loginedUser.UserId); // 새로운 메시지 확인
+
+            if (HasNewMessages() && !isNotificationShown)
+            {
+                isNotificationShown = true;
+                ShowNotification("새로운 메시지가 도착했습니다.");
+            }
+        }
+
+        private bool HasNewMessages()
+        {
+            try
+            {
+                DBManager dbManager = DBManager.GetInstance();
+                string query = $"SELECT COUNT(*) FROM message WHERE recipient_id = {loginedUser.UserId} AND state = 1";
+                DataTable result = dbManager.FindDataTable(query);
+
+                if (result != null && result.Rows.Count > 0)
+                {
+                    int messageCount = Convert.ToInt32(result.Rows[0][0]);
+                    return messageCount > 0;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"새로운 메시지 확인 중 오류 발생: {ex.Message}");
+                return false;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DBManager dbManager = DBManager.GetInstance();
+            if (listBox1.SelectedIndex != -1)
+            {
+                string selectedItem =
+                    listBox1.SelectedItem.ToString();
+                string messageContent = GetMessageContent(selectedItem); //
+                richTextBox1.Text = messageContent;
+
+                string[] parts = selectedItem.Split(new string[] { "제목: ", ", 발신자: ", ", 상태: " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string title = parts[0];
+                string senderName = parts[1];
+                string status = parts[2];
+
+                if (status == "안 읽음")
+                {
+                    dbManager.InitDBManager();
+                    string senderIdQuery = $"SELECT 사원ID FROM 사원 WHERE 이름 = '{senderName}'";
+                    try
+                    {
+                        int senderId = Convert.ToInt32(dbManager.FindDataTable(senderIdQuery).Rows[0]["사원ID"]);
+                        string updateQuery = $"UPDATE message SET state = 0 WHERE title = '{title}' AND sender_id = {senderId}";
+                        dbManager.ExecuteNonQueury(updateQuery);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"상태 변경 중 오류 발생: {ex.Message}");
+                    }
+                    dbManager.CloseConnection();
+                }
+                ShowReceivedMessagesForUser(loginedUser.UserId);
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private string GetMessageContent(string selectedItem)
+        {
+            DBManager dbManager = DBManager.GetInstance();
+
+            string[] parts = selectedItem.Split(new string[] { "제목: ", ", 발신자: ", ", 상태: " }, StringSplitOptions.RemoveEmptyEntries);
+            string title = parts[0];
+            string senderName = parts[1];
+            string status = parts[2];
+
+            string senderIdQuery = $"SELECT 사원ID FROM 사원 WHERE 이름 = '{senderName}'";
+
+            try
+            {
+                int senderId = Convert.ToInt32(dbManager.FindDataTable(senderIdQuery).Rows[0]["사원ID"]);
+                string query = $"SELECT content FROM message WHERE title = '{title}' AND sender_id = {senderId}";
+
+                DataTable dataTable = dbManager.FindDataTable(query);
+                if (dataTable.Rows.Count > 0)
+                {
+                    string messageContent = dataTable.Rows[0]["content"].ToString();
+                    return messageContent;
+                }
+                else
+                {
+                    return "해당하는 내용을 찾을 수 없습니다.";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"메시지 내용을 가져오는 중 오류 발생: {ex.Message}");
+                return "오류 발생";
+            }
+        }
+
+        public void PopulateComboBox()
+        {
+            DBManager dbManager = DBManager.GetInstance();
+            string query = "SELECT 사원ID, 부서이름, 이름, 직급 FROM 사원";
+
+            try
+            {
+                DataTable dataTable = dbManager.FindDataTable(query);
+                comboBox1.Items.Clear();
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    string item = $"{row["부서이름"]} {row["이름"]} {row["직급"]} {row["사원ID"]}";
+                    comboBox1.Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"데이터 로드 중 오류 발생: {ex.Message}");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int recipientId = ExtractRecipientId();
+            string title = textBox1.Text;
+            string content = richTextBox1.Text;
+
+            if (recipientId != -1 && !string.IsNullOrEmpty(title) && !string.IsNullOrEmpty(content))
+            {
+                SendMessage(recipientId, title, content);
+            }
+            else
+            {
+                MessageBox.Show("모든 필드를 입력하세요.");
+            }
+        }
+
+        private void SendMessage(int recipientId, string title, string content)
+        {
+            DBManager dbManager = DBManager.GetInstance();
+            dbManager.InitDBManager();
+            try
+            {
+                string query = "INSERT INTO message (sender_id, recipient_id, title, content, sent_time, state) " +
+                "VALUES (@senderId, @recipientId, @title, @content, NOW(), @state)";
+
+                MySqlCommand cmd = new MySqlCommand(query, dbManager.Connection);
+                cmd.Parameters.AddWithValue("@senderId", loginedUser.UserId); //자동으로 받아지게////////////////////////////////////////////////////////////////////////////////////////////////
+                cmd.Parameters.AddWithValue("@recipientId", recipientId);
+                cmd.Parameters.AddWithValue("@title", title);
+                cmd.Parameters.AddWithValue("@content", content);
+                cmd.Parameters.AddWithValue("@state", 1);
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                if (rowsAffected > 0)
+                {
+                    MessageBox.Show("메시지가 전송되었습니다.");
+                }
+                else
+                {
+                    MessageBox.Show("메시지 전송에 실패했습니다.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"메시지 전송 중 오류 발생: {ex.Message}");
+            }
+            dbManager.CloseConnection();
+        }
+        private void ShowReceivedMessagesForUser(string userId)
+        {
+            DBManager dbManager = DBManager.GetInstance();
+            try
+            {
+                string query = $"SELECT m.title, e.이름 as sender_name, m.state " +
+                               $"FROM message m " +
+                               $"JOIN 사원 e ON m.sender_id = e.사원ID " +
+                               $"WHERE recipient_id = {userId}";
+                DataTable dataTable = dbManager.FindDataTable(query);
+
+                listBox1.Items.Clear();
+
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    string messageTitle = row["title"].ToString();
+                    string senderName = row["sender_name"].ToString();
+
+                    string status = "알 수 없음";
+                    if (row["state"] != DBNull.Value)
+                    {
+                        int state = Convert.ToInt32(row["state"]);
+                        status = state == 1 ? "안 읽음" : "읽음";
+                    }
+
+                    listBox1.Items.Add($"제목: {messageTitle}, 발신자: {senderName}, 상태: {status}");
+
+                    if (status == "안 읽음")
+                    {
+                        ShowNotification($"새로운 메시지: {messageTitle}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"메시지 가져오기 중 오류 발생: {ex.Message}");
+            }
+        }
+
+        private int ExtractRecipientId()
+        {
+            int recipientId = -1;
+
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedItem = comboBox1.SelectedItem.ToString();
+                string[] selectedParts = selectedItem.Split(' ');
+
+                if (int.TryParse(selectedParts.Last(), out recipientId))
+                {
+                    return recipientId;
+                }
+            }
+
+            return recipientId;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string searchText = textBox2.Text;
+            string query = $"SELECT m.title, e.이름 as sender_name, m.state " +
+                           $"FROM message m " +
+                           $"JOIN 사원 e ON m.sender_id = e.사원ID " +
+                           $"WHERE m.title LIKE '%{searchText}%' OR m.content LIKE '%{searchText}%' OR e.이름 LIKE '%{searchText}%'";
+
+            DataTable dataTable = dbManager.FindDataTable(query);
+            listBox1.Items.Clear();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string messageTitle = row["title"].ToString();
+                string senderName = row["sender_name"].ToString();
+
+                string status = "알 수 없음";
+                if (row["state"] != DBNull.Value)
+                {
+                    int state = Convert.ToInt32(row["state"]);
+                    status = state == 1 ? "안 읽음" : "읽음";
+                }
+
+                listBox1.Items.Add($"제목: {messageTitle}, 발신자: {senderName}, 상태: {status}");
+            }
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        private async void ShowNotification(string message)
+        {
+            if (!isNotificationShown) // 팝업이 떠있지 않을 때만 실행
+            {
+                isNotificationShown = true; // 팝업이 떠있음을 설정
+
+                DialogResult result = MessageBox.Show(message, "새로운 메시지 도착", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                if (result == DialogResult.OK) // 사용자가 확인 버튼을 누르면
+                {
+                    await Task.Delay(3000); // 3초 대기
+
+                    // 팝업이 닫힌 후에는 팝업이 더이상 떠있지 않도록 설정
+                    isNotificationShown = false;
+
+                    // 새로운 알림을 표시하기 위해 메시지 목록을 갱신
+                    ShowReceivedMessagesForUser(loginedUser.UserId);
+                }
+            }
         }
     }
 }
